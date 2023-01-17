b5440097d23e7731a788214d01e5ee43d391b732
dangabriel55 <cena_re_ra_dane@yahoo.com>
2023-01-17 13:23:04 +0200

    first commit

---
+++ BlazorServerCRUD/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerCRUD.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>().HasData(
+                new Game { Id = 1, Name = "Half Life 2", Developer = "Valve", Release = new DateTime(2004, 11, 16), } ,
+                new Game { Id = 2, Name = "Day Ten", Developer = "Lucas Arts", Release = new DateTime(1993, 10, 26), }
+             );
+        }
+        public DbSet<Game> Games => Set<Game>();
+    }
+}
